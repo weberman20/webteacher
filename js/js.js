beb78977b15d770e9a783128609ab39ca9801d28
weberman20 <106586292+weberman20@users.noreply.github.com>
2022-05-31 16:26:44 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -4,9 +4,3 @@ $(document).ready(function() {
 		$('body').toggleClass('lock');
 	})
 });
-
-$(document).ready(function()
-	$('.header_burger').touchstart(function(event) {
-		$('header_burger,.header_menu').toggleClass('active');
-		$('body').toggleClass('lock');
-	})
